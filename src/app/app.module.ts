05a887e9b86dab41146a0ba611d89e5fbccc1847
mgechev <mgechev@gmail.com>
2019-09-13 14:02:44 -0700

    Repro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import 'devextreme-angular';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
